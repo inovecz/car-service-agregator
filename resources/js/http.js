5a55be0bd98468e4d672ffba687939ef5a8b2d40
kozusnik.jan <kozusnik.jan@gmail.com>
2023-11-28 08:20:10 +0100

    Install axios

---
+++ resources/js/http.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+
+let domain = 'agregator.lan';
+
+let myAxios = axios.create({
+    baseURL: 'http://' + domain + '/api',
+});
+
+
+let self = this;
+
+/* myAxios.interceptors.response.use(function (response) {
+    if (response.status == 200 && response.data && response.data.message && response.data.message.length > 0) {
+        Swal.fire({
+            type: 'success',
+            icon: 'success',
+            text: response.data.message
+        });
+    }
+
+    return response;
+}, async function (error) {
+    if (error.response) {
+        let response = error.response;
+        const code = response.status;
+        const errorMessage = response.data.message;
+        const originalConfig = error.config;
+
+        if(response.data.message) {
+            Swal.fire({
+                type: 'error',
+                message: response.data.message
+            });
+        }
+
+    }
+    return Promise.reject(error);
+}); */
+
+
+export default myAxios;
