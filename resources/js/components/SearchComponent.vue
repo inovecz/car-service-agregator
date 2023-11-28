7611b6f8ebb798d633f522a3db98f2a092a016d5
kozusnik.jan <kozusnik.jan@gmail.com>
2023-11-28 10:41:48 +0100

    Add table with search result

--- resources/js/components/SearchComponent.vue
+++ resources/js/components/SearchComponent.vue
@@ -31,7 +31,65 @@
             </div>
           </div>
         </form>
-        <div v-if="products">Tu budou produkty<br />{{ products }}</div>
+        <div v-if="products.length" class="mt-3">
+          <div class="grid grid-flow-col">
+            <div
+              class="not-prose relative bg-slate-50 rounded-xl overflow-hidden dark:bg-slate-800/25"
+            >
+              <div
+                style="background-position: 10px 10px"
+                class="absolute inset-0 bg-grid-slate-100 [mask-image:linear-gradient(0deg,#fff,rgba(255,255,255,0.6))] dark:bg-grid-slate-700/25 dark:[mask-image:linear-gradient(0deg,rgba(255,255,255,0.1),rgba(255,255,255,0.5))]"
+              ></div>
+              <div class="relative rounded-xl overflow-auto">
+                <div class="shadow-sm overflow-hidden my-8">
+                  <table class="border-collapse table-auto w-full text-sm">
+                    <thead>
+                      <tr>
+                        <th
+                          class="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left"
+                        >
+                          Kód produktu
+                        </th>
+                        <th
+                          class="border-b dark:border-slate-600 font-medium p-4 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left"
+                        >
+                          Název produktu
+                        </th>
+                        <th
+                          class="border-b dark:border-slate-600 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left"
+                        >
+                          Dodavatel
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody class="bg-white dark:bg-slate-800">
+                      <tr v-for="product in products">
+                        <td
+                          class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400"
+                        >
+                          {{ product.product_code }}
+                        </td>
+                        <td
+                          class="border-b border-slate-100 dark:border-slate-700 p-4 text-slate-500 dark:text-slate-400"
+                        >
+                          {{ product.product_name }}
+                        </td>
+                        <td
+                          class="border-b border-slate-100 dark:border-slate-700 p-4 pr-8 text-slate-500 dark:text-slate-400"
+                        >
+                          {{ product.supplier_name }}
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+              <div
+                class="absolute inset-0 pointer-events-none border border-black/5 rounded-xl dark:border-white/5"
+              ></div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
